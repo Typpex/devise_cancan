f6340d5a6b6306344642dcd580f83bb2c3ec9d6e
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-08-21 17:50:43 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestDeviseCancan::Application.config.secret_token = 'de1f7e854add4ad861238b10c3091d7835222bb54023e37649ee648b28f95830323640099eb0fea6faaca77c3058a728c4a8f2bd67deeeb5788739a0df2b6be8'
